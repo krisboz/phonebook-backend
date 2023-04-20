777992663f408025433b203530a6597e4000d4e5
krisboz <kristijan.bozinovic1@gmail.com>
2023-04-20 11:09:53 +0200

    Phonebook backend

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+/**
+const requestLogger = (request, response, next) => {
+  console.log(`Method: ${request.method}`);
+  console.log(`Path: ${request.path}`);
+  console.log(`Body: ${request.body}`);
+  console.log("--------------------");
+  next();
+};
+ */
+morgan.token("user-type", function (req, res) {
+  console.log({});
+  return JSON.stringify(req.body);
+});
+app.use(morgan(":method :url :status  :user-type - :response-time ms"));
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "Custom unknonw endpoint" });
+};
 
 app.use(express.json());
+app.use(cors());
+//app.use(requestLogger);
 
 let people = [
   {
@@ -25,18 +46,18 @@ let people = [
     number: "39-23-6423122",
   },
 ];
-
+//GET ALL
 app.get("/api/people", (request, response) => {
   response.json(people);
 });
-
+//GET INFO
 app.get("/info", (request, response) => {
   const time = new Date();
   response.setHeader("Content-Type", "text/html");
   response.send(`<p>The phonebook has info on ${people.length} people</p>
     <p>${time} </p>`);
 });
-
+//GET PERSON
 app.get("/api/people/:id", (request, response) => {
   const id = Number(request.params.id);
   const person = people.filter((person) => person.id === id);
@@ -46,14 +67,14 @@ app.get("/api/people/:id", (request, response) => {
     response.status(404).end();
   }
 });
-
+//DELETE PERSON
 app.delete("/api/people/:id", (request, response) => {
   const id = Number(request.params.id);
   people = people.filter((person) => person.id !== id);
 
   response.status(404).end();
 });
-
+//POST PERSON
 app.post("/api/people", (request, response) => {
   const id = Math.floor(Math.random() * 100000);
   const body = request.body;
@@ -86,9 +107,11 @@ app.post("/api/people", (request, response) => {
     });
   }
 
-  people.concat(person);
+  people = [...people, person];
   response.json(person);
 });
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT);
