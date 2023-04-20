0c1bda42a8fb0a2c7ed4fc1fb521d74116101f87
krisboz <kristijan.bozinovic1@gmail.com>
2023-04-20 11:12:09 +0200

    Phonebook backend

--- index.js
+++ index.js
@@ -113,6 +113,6 @@ app.post("/api/people", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server is running on port ${PORT}`);
